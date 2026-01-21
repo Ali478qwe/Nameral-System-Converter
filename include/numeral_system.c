f369660527a2cd7c4b24d8727859953c319a6d23
Ali478qwe <a667908@gmail.com>
2026-01-21 11:22:29 +0330

    added function for convert decimal-binary-hex-octal

--- include/numeral_system.c
+++ include/numeral_system.c
@@ -21,6 +21,42 @@
 #include <ctype.h>
 #include <string.h>
 
+
+//find flag : #-0b-0x-0b
+static codeflag_t findCodeFlag(char character[]){
+   int len = strlen(character);
+
+   if(character[0] == '#')
+     return Sharp;
+   else if(len>= 2 && character[0] == '0' && tolower(character[1]) == 'x')
+     return Hexadecimal;
+   else if(len>= 2 && character[0] == '0' && tolower(character[1]) == 'b')
+     return Binary;
+   else if(len>= 2 && character[0] == '0' && tolower(character[1]) == 'o')
+     return Octal;
+}
+
+static int startCodeIndex(codeflag_t flag){
+     switch (flag)
+     {
+     case Sharp:
+          return 1;
+          break;
+     case Hexadecimal:
+          return 2;
+          break;
+     case Binary:
+          return 2;
+          break;
+     case Octal:
+          return 2;
+          break;
+     default:
+          return 0;
+          break;
+     }
+}
+
 static int hexCharToInt(char * character){
     if(*character >= '0' && *character <= '9') 
          return *character - '0';
@@ -32,18 +68,21 @@ static int hexCharToInt(char * character){
          return -1;
 }
 
+char intToHexChar(int number){
+     if(number >= 0 && number <= 9) 
+         return (char)(number + 48);
+    else if(number >= 10 && number <= 15) 
+         return (char)(number + 55);
+    else
+         return -1;
 
-unsigned long long hexToDecimal(char character[]){
+}
 
-   int len = strlen(character);
-   int start = 0;
+unsigned long long hexToDecimal(char hex[]){
+
+   int len = strlen(hex);
+   int start = startCodeIndex(findCodeFlag(hex));
    
-   //find flag
-   if(character[0] == '#')
-      start = 1;
-   else if(len> 2 && character[0] == '0' && tolower(character[1]) == 'x')
-      start = 2;
-      
 
     unsigned long long result = 0;
     int digit = 0;
@@ -51,9 +90,9 @@ unsigned long long hexToDecimal(char character[]){
     //convert type
     for(int i = start;i < len;i++){
         
-      if(isxdigit(character[i])){
+      if(isxdigit(hex[i])){
 
-          digit = hexCharToInt(&character[i]);
+          digit = hexCharToInt(&hex[i]);
           result = (result * 16) + digit;//Horners Law
 
       }else
@@ -67,3 +106,222 @@ unsigned long long hexToDecimal(char character[]){
     }
     return result;
 }
+
+void hexToBinary(char hex[],char * output){
+     __u_int len = strlen(hex);
+     __u_int start = startCodeIndex(findCodeFlag(hex));
+     __u_int out_index = 2;
+
+     output[0] = '0';
+     output[1] = 'b';
+
+     for(__u_int i = start;i < len;i++){
+
+          if(isxdigit(hex[i])){
+
+               __u_int value = hexCharToInt(&hex[i]);
+
+               for(__uint8_t bit = 3; bit < 4;bit--){
+                    output[out_index++] = ((value >> bit) & 1)?'1':'0';
+               }
+
+               // if(i < len - 1){
+               //      output[out_index++] = ' ';
+               // }
+          }
+     }
+     
+     output[out_index] = '\0';
+
+}
+
+void hexToOctal(char hex[],char * output){
+     unsigned long long Decimal = hexToDecimal(hex);
+     decimalToOctal(Decimal,output);
+}
+
+void decimalToHex(unsigned int number,char * output){
+     char temp[20];
+     int i = 0;
+
+     if(number == 0){
+       output[0] = '0';
+       output[1] = 'x';
+       output[2] = '0';
+       output[3] = '\0';
+       return;
+     }
+
+     while(number > 0){
+       int remainder = number % 16;
+       temp[i] = intToHexChar(remainder);
+       number = number / 16;
+       i++;
+     }
+
+     output[0] = '0';
+     output[1] = 'x';
+
+     for(int j = 0; j < i;j++){
+       output[j+2] = temp[i-j-1];
+     }
+
+     output[i+2] = '\0';
+     
+}
+
+void decimalToBinary(unsigned int number,char * output){
+     char temp[33]; // 32 bit + nullbit
+     int i = 0;
+
+     if(number == 0){
+       strcpy(output,"0b0");
+       return;
+     }
+
+     while(number > 0){
+       temp[i++] = (number % 2) + '0' ;
+       number /= 2;
+     }
+
+     output[0] = '0';
+     output[1] = 'b';
+
+     for(int j = 0; j < i;j++){
+       output[j+2] = temp[i-j-1];
+     }
+
+     output[i+2] = '\0';
+     
+}
+
+void decimalToOctal(unsigned int number,char * output){
+     char temp[60];
+     int i = 0;
+
+     if(number == 0){
+       strcpy(output,"0o0");
+       return;
+     }
+
+     while(number > 0){
+       temp[i++] = (number % 8) + '0' ;
+       number /= 8;
+     }
+
+     output[0] = '0';
+     output[1] = 'o';
+
+     for(int j = 0; j < i;j++){
+       output[j+2] = temp[i-j-1];
+     }
+
+     output[i+2] = '\0';
+}
+
+unsigned long long binaryToDecimal(char binary[]){
+     int len = strlen(binary);
+     int start = startCodeIndex(findCodeFlag(binary));
+     unsigned long long result = 0;
+
+     for(int i = start; i < len;i++){
+
+          if(binary[i] == ' ') continue;
+
+          result <<= 1;
+
+          if(binary[i] == '1'){
+               result |= 1;
+          }else if(binary[i] == '0'){
+
+          }else{
+               return -1;
+          }
+     }
+     return result;
+}
+
+void binaryToHex(char binary[],char * output){
+     char clean_bit[100];
+     int len = strlen(binary);
+
+     __uint8_t start = startCodeIndex(findCodeFlag(binary));    
+     __uint8_t clean_start = 0;  
+     __uint8_t padding = 0;
+     __uint8_t hex_index = 2;
+
+     for(__uint8_t i = start; i < len;i++){
+          // if(binary[i] == ' ')continue;
+          clean_bit[clean_start++] = binary[i];
+     }
+
+     clean_bit[clean_start] = '\0';
+
+     __uint8_t clean_len = strlen(clean_bit);
+
+
+     padding = (4 - (clean_len % 4)) % 4;
+     
+     output[0] = '0';
+     output[1] = 'x';
+
+     for(__uint8_t i= 0; i< clean_len + padding;i += 4){
+          __uint8_t  value = 0; 
+
+          for(__uint8_t j = 0; j < 4;j++){
+               __int8_t bit_pos = i + j - padding;
+               char bit_char;
+
+               bit_char  = (bit_pos < 0)?'0':clean_bit[bit_pos];
+
+               value = (value << 1) | (bit_char - '0');
+          }
+
+          output[hex_index++] = intToHexChar(value);
+     }
+     output[hex_index] = '\0';
+}
+
+void binaryToOctal(char binary[],char * output){
+     char clean_bit[100];
+     int len = strlen(binary);
+
+     __uint8_t start = startCodeIndex(findCodeFlag(binary));    
+     __uint8_t clean_start = 0;  
+     __uint8_t padding = 0;
+     __uint8_t hex_index = 2;
+
+     for(__uint8_t i = start; i < len;i++){
+          // if(binary[i] == ' ')continue;
+          clean_bit[clean_start++] = binary[i];
+     }
+
+     clean_bit[clean_start] = '\0';
+
+     __uint8_t clean_len = strlen(clean_bit);
+
+
+     padding = (3 - (clean_len % 3)) % 3;
+     
+     output[0] = '0';
+     output[1] = 'o';
+
+     for(__uint8_t i= 0; i< clean_len + padding;i += 3){
+          __uint8_t  value = 0; 
+
+          for(__uint8_t j = 0; j < 3;j++){
+               __int8_t bit_pos = i + j - padding;
+               char bit_char;
+
+               bit_char  = (bit_pos < 0)?'0':clean_bit[bit_pos];
+
+               value = (value << 1) | (bit_char - '0');
+          }
+
+          output[hex_index++] = intToHexChar(value);
+     }
+     output[hex_index] = '\0';
+}
+
+
+
--- include/numeral_system.h
+++ include/numeral_system.h
@@ -4,9 +4,33 @@
 #define HEX_CONVERTER_VERSION "V1.0-PROTOTYPE"
 #define VERSION_DATE "2026-01-20"
 
+#include <stdbool.h>
+
+typedef enum {
+     Sharp,
+     Decimal,
+     Binary,
+     Octal,
+     Hexadecimal
+}codeflag_t;
+
+
 //int hexCharToInt(char * character);
+// char intToHexChar(int number);
 
-unsigned long long hexToDecimal(char * character);
+unsigned long long hexToDecimal(char * hex);
+void hexToBinary(char hex[],char * output);
+void hexToOctal(char hex[],char * output);
 
-#endif 
+void decimalToHex(unsigned int number,char * output);
+void decimalToBinary(unsigned int number,char * output);
+void decimalToOctal(unsigned int number,char * output);
+
+unsigned long long binaryToDecimal(char binary[]);
+void binaryToHex(char binary[],char * output);
+void binaryToOctal(char binary[],char * output);
+
+
+
+#endif
 
